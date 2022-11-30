e3d84a2ed6d04f990270e017fd643b97c4c20769
TaeHun Kim <no1kim1026@gmail.com>
2022-11-30 12:28:32 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView ## 여러 포스트 목록 페이지를 만들기 위해 ListView 가져오기
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+# 여러 포스트 목록 페이지를 만들때 사용할 수 있는 기능
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+   
+   
+
+   # ListView를 활용한 방법에서 PostList 불러오는 방법 1
+   #  template_name = 'blog/index.html'
+
+
+
+
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts' : posts,
+#        }
+#    )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
