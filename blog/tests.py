21b76e498690dc8d914dc0045bc00dcc07a3593b
TaeHun Kim <no1kim1026@gmail.com>
2022-12-01 14:41:30 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/tests.py
+++ blog/tests.py
@@ -36,27 +36,32 @@ class TestView(TestCase):
         self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
         # 3.1  포스트가 2개 있다면
+    def test_post_detail(self):
+
         post_001 = Post.objects.create(
             title = '첫 번째 포스트입니다.',
             content='Hello World. We are the world',
         )
-        post_002 = Post.objects.create(
-            title='두 번째 포스트입니다.',
-            content='1등이 전부는 아니잖아요?',
-        )
-        self.assertEqual(Post.objects.count(), 2)
+        self.assertEqual(post_001.get_absolute_url(), '/blog/')
 
         # 3.2 포스트 목록 페이지를 새로고침 했을 때
-        response = self.client.get('/blog/')
-        soup = BeautifulSoup(response.content, 'html.parser')
+        response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        navbar=soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
 
         # 3.3 main area에 포스트 2개의 제목이 존재한다.
         main_area = soup.find('div', id='main-area')
-        self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
 
         # 3.4 '아직 게시물이 없습니다.' 라는 문구가 더 이상 나타나지 않는다.
-        self.assertIn('아직 게시물이 없습니다.', main_area.text)
+        self.assertIn(post_001.content, main_area.text)
+
+
+
 
 
